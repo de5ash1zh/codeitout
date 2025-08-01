5afb9ac02cf783e0fd3dd92da6b5fd12724989da
Devashish <devashish@Devashishs-MacBook-Air.local>
2025-08-01 23:47:30 +0530

    db connect and auth routes init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to codeitout");
+});
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`serving on port: ${port}`);
+});
